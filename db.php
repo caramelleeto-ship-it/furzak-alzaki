da64733cdac9ebc483ddb67ff704abe720c023c4
caramelleeto-ship-it <caramelleeto@gmail.com>
2026-04-27 22:23:55 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,20 +1,14 @@
 <?php
+$DB_HOST = getenv('DB_HOST') ?: "localhost";
+$DB_PORT = (int)(getenv('DB_PORT') ?: 3307);
+$DB_NAME = getenv('DB_NAME') ?: "recycle_ai";
+$DB_USER = getenv('DB_USER') ?: "root";
+$DB_PASS = getenv('DB_PASS') ?: "";
 
-$DB_HOST = "localhost";
-$DB_PORT = 3307;            
-$DB_NAME = "recycle_ai";   
-$DB_USER = "root";         
-$DB_PASS = "";              
-
-/* إنشاء الاتصال */
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 
-/* فحص الاتصال */
 if ($conn->connect_error) {
-    die(json_encode([
-        "success" => false,
-        "error"   => "فشل الاتصال بقاعدة البيانات: " . $conn->connect_error
-    ]));
+    die("فشل الاتصال: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
